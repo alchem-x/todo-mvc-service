74c59362eb03a1398ca225f6e74b6c3c1b4d4260
MRongM <1399316847@qq.com>
2021-08-27 16:22:26 +0800

    perl init

---
+++ perl/service.pl
@@ -0,0 +1,17 @@
+package service;
+
+sub do_post{
+
+}
+
+sub do_get{
+
+}
+
+sub do_put{
+
+}
+
+sub do_delete{
+
+}
---
+++ perl/store.pl
@@ -0,0 +1,27 @@
+package store;
+
+my $file_name = "todo.json";
+
+sub read_file{
+
+}
+
+sub save_file{
+
+}
+
+sub create{
+
+}
+
+sub list{
+
+}
+
+sub update{
+
+}
+
+sub delete{
+
+}
